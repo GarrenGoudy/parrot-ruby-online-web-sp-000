3d5ac5dd9b37c2829a5700b5b5892697b327ff61
Garren Goudy <garren.goudy@yahoo.com>
2021-06-08 15:53:44 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,14 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot
+  puts "Squawk!"
+  return "Squawk!"
+end
+
+def parrot_phrase
+  puts parrot("pretty bird!")
+  return parrot("pretty bird!")
+end
+
+phrase = parrot
